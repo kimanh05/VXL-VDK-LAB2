d21a2e41d70da3f60d441d5fdda466e6b5820170
KIM_ANH\kiman <anh.nguyen2510@hcmut.edu.vn>
2025-10-02 19:24:27 +0700

    Update Exercise2

--- LAB2_EX/Core/Src/main.c
+++ LAB2_EX/Core/Src/main.c
@@ -200,14 +200,17 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|EN0_Pin|EN1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, DOT_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, SEG0_Pin|SEG1_Pin|SEG2_Pin|SEG3_Pin
                           |SEG4_Pin|SEG5_Pin|SEG6_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin EN0_Pin EN1_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|EN0_Pin|EN1_Pin;
+  /*Configure GPIO pins : DOT_Pin LED_RED_Pin EN0_Pin EN1_Pin
+                           EN2_Pin EN3_Pin */
+  GPIO_InitStruct.Pin = DOT_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -252,28 +255,51 @@ void display7SEG(int num) {
 
 
 int counter = 50;
+int countDot = 100;
 int flag = 0;
-void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim )
-{
-   if (flag == 0) {
-	   HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
-	   HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
-	   display7SEG(1);
-   }
-   if (flag == 1) {
-   	   HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
-   	   HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
-   	   display7SEG(2);
-      }
 
-   counter--;
-   if( counter <= 0) {
-      counter = 50;
-      if (flag == 0) flag = 1;
-      else flag = 0;
-      HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-   }
+void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim)
+{
+    HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
+    HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+    HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
+    HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
+
+    switch(flag) {
+        case 0:
+            HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+            display7SEG(1);
+            break;
+        case 1:
+            HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+            display7SEG(2);
+            break;
+        case 2:
+            HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
+            display7SEG(3);
+            break;
+        case 3:
+            HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
+            display7SEG(0);
+            break;
+    }
+
+    counter--;
+    countDot--;
+
+    if(counter <= 0) {
+        counter = 50;
+        flag++;
+        if(flag == 4) flag = 0;
+        HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+    }
+
+    if(countDot <= 0) {
+        countDot = 100;
+        HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+    }
 }
+
 /* USER CODE END 4 */
 
 /**
